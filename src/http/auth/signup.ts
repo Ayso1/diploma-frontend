15bc7c9cf3c8702753b2c199e9a237add0f480b0
Max Reznichenko <reznichenko.max9@gmail.com>
2022-08-05 17:46:06 +0200

    signUp page with loading

--- src/http/auth/signup.ts
+++ src/http/auth/signup.ts
@@ -1,25 +1,13 @@
 import axios from 'axios';
 import config from '../../config';
 async function doPostRequest(data) {
-  let res = await axios
-    .post(`${config.apiUrl}/user`, {
-      email: data.email,
-      password: data.password,
-      isEmailVerified: true,
-      firstName: data.firstName,
-      lastName: data.lastName,
-    })
-    .then((res) => {
-      console.log(res.data);
-      return true;
-    })
-    .catch((error) => {
-      console.error(error);
-      return false;
-    });
-  if (res === false) {
-    return false;
-  }
+  let res = await axios.post(`${config.apiUrl}/user`, {
+    email: data.email,
+    password: data.password,
+    isEmailVerified: true,
+    firstName: data.firstName,
+    lastName: data.lastName,
+  });
 }
 
 export default doPostRequest;
